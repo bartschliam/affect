a7c920b6b7cab8aba51872fa4094f64389d3b5fd
Liam Bartsch <bartschl@tcd.ie>
2023-03-07 10:33:29 +0000

    addded formatting of data for future analysis

--- data_collection/main.py
+++ data_collection/main.py
@@ -19,35 +19,43 @@ def query_api():
         password=password, 
         username=reddit_username)
     subreddits = [
-        'askreddit',
-        'socialskills',
         'gaming',
-        'movies', 
-        'facepalm',
         'funny',
         'aww',
         'bunnies', 
-        'lgbt'
+        'lgbt',
+        'socialskills',
+        'movies', 
+        'facepalm',
     ]
     num_posts = 100
+    found = False
     last_request_time = time.time()
     titles_and_emojis = []
     for subreddit in subreddits:
-        i = 0
-        for post in reddit.subreddit(subreddit).new(limit=num_posts):
-            i += 1
-            print(subreddit, i)
+        print(f'Subreddit: {subreddit}')
+        for i, post in enumerate(reddit.subreddit(subreddit).new(limit=num_posts)):
+            print(f"Index: {i}, Post: {post.title}")
             if last_request_time is not None and time.time() - last_request_time < 1:
                 time_to_wait = 1 - (time.time() - last_request_time)
-                print(f"Waiting for {time_to_wait:.1f} seconds before next request...")
+                #print(f"Waiting for {time_to_wait:.1f} seconds before next request...")
                 time.sleep(time_to_wait)
-        
             title = post.title
             emojis = [c for c in title if c in emoji.EMOJI_DATA]
             if emojis:
-                titles_and_emojis.append((title, emojis))
+                #titles_and_emojis.append((title, emojis))
+                if emoji in titles_and_emojis:
+                    titles_and_emojis[emojis]['frequency'] += 1
+                else:
+                    titles_and_emojis[emojis] = {}
+                    titles_and_emojis[emojis]['frequency'] = 1
+                titles_and_emojis[emojis]['subreddits'].add(subreddit)
+                found = True
             last_request_time = time.time()
-    print(titles_and_emojis)
+            if found:
+                break
+        if found:
+            break
     with open('titles_and_emojis.json', 'w') as f:
         json.dump(titles_and_emojis, f)
     return
@@ -55,7 +63,6 @@ def query_api():
 def format_json():
     with open('titles_and_emojis.json', 'r') as f:
         data = json.load(f)
-
     formatted_data = {}
     for item in data:
         formatted_data[item[0]] = item[1]
