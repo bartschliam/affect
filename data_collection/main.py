f79bd263c7789afbf741a630499910d2cdc6e9ce
Liam Bartsch <bartschl@tcd.ie>
2023-03-07 12:57:38 +0000

    updated gitignore with vs code settings, testing different subreddits and creating mapping

--- data_collection/main.py
+++ data_collection/main.py
@@ -6,6 +6,7 @@ import emoji
 import time
 import json
 
+
 def query_api():
     load_dotenv()
     client_id = os.getenv('client_id')
@@ -13,25 +14,26 @@ def query_api():
     password = os.getenv('password')
     reddit_username = os.getenv('reddit_username')
     reddit = praw.Reddit(
-        client_id=client_id, 
-        client_secret=client_secret, 
-        user_agent='Affect Analysis Bot for various subreddits v1.0 by u/username', 
-        password=password, 
+        client_id=client_id,
+        client_secret=client_secret,
+        user_agent='Affect Analysis Bot for various subreddits v1.0 by u/username',
+        password=password,
         username=reddit_username)
     subreddits = [
+        'cloudwater',
         'gaming',
         'funny',
         'aww',
-        'bunnies', 
+        'bunnies',
         'lgbt',
         'socialskills',
-        'movies', 
+        'movies',
         'facepalm',
     ]
     num_posts = 100
     found = False
     last_request_time = time.time()
-    titles_and_emojis = []
+    titles_and_emojis = {}
     for subreddit in subreddits:
         print(f'Subreddit: {subreddit}')
         for i, post in enumerate(reddit.subreddit(subreddit).new(limit=num_posts)):
@@ -43,14 +45,18 @@ def query_api():
             title = post.title
             emojis = [c for c in title if c in emoji.EMOJI_DATA]
             if emojis:
+                for item in emojis:
+                    emoji_hex = item
+                    if emoji_hex in titles_and_emojis:
+                        titles_and_emojis[emoji_hex]['frequency'] += 1
+                        titles_and_emojis[emoji_hex]['subreddits'].append(subreddit)
+                    else:
+                        titles_and_emojis[emoji_hex] = {}
+                        titles_and_emojis[emoji_hex]['frequency'] = 1
+                        titles_and_emojis[emoji_hex]['subreddits'] = [subreddit]
+                #found = True
                 #titles_and_emojis.append((title, emojis))
-                if emoji in titles_and_emojis:
-                    titles_and_emojis[emojis]['frequency'] += 1
-                else:
-                    titles_and_emojis[emojis] = {}
-                    titles_and_emojis[emojis]['frequency'] = 1
-                titles_and_emojis[emojis]['subreddits'].add(subreddit)
-                found = True
+
             last_request_time = time.time()
             if found:
                 break
@@ -60,21 +66,23 @@ def query_api():
         json.dump(titles_and_emojis, f)
     return
 
+
 def format_json():
     with open('titles_and_emojis.json', 'r') as f:
         data = json.load(f)
     formatted_data = {}
-    for item in data:
-        formatted_data[item[0]] = item[1]
+    # for item in data:
+    #     formatted_data[item[0]] = item[1]
 
     print(formatted_data)
     return
 
+
 def main():
-    #query_api()
+    query_api()
     format_json()
     return
 
+
 if __name__ == '__main__':
     main()
-
